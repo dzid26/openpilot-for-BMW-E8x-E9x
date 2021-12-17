81fd94de7d85db4383cd96b3b491cb1b06554926
dzid26 <dzidmail@gmail.com>
2021-12-17 13:53:47 +0000

    BMW dbc with checksums instead of counters. bmwcan.py updated.
    (cherry picked from commit 364545f1164c546212690960da7b9cfcc37e5f49)

--- selfdrive/car/bmw/bmwcan.py
+++ selfdrive/car/bmw/bmwcan.py
@@ -5,35 +5,47 @@ def create_steer_command(packer, steer_angle_chg):
     values = {"steer_angle_chg": steer_angle_chg}
     return packer.make_can_msg("Dummy", 1, values)
 
+def calc_checksum_4bit(work_data, msg_id): # 0x130
+  checksum = msg_id
+  for byte in work_data: #checksum is stripped from the dat
+    checksum += byte     #add up all the bytes
+
+  checksum = (checksum & 0xFF) + (checksum >> 8); #add upper and lower Bytes
+  checksum &= 0xFF #throw away anything in upper Byte  
+
+  checksum = (checksum & 0xF) + (checksum >> 4); #add first and second nibble
+  checksum &= 0xF; #throw away anything in upper nibble
+  return checksum
+
+def calc_checksum_8bit(work_data, msg_id): # 0xb8 0x1a0 0x19e 0xaa 0xbf
+  checksum = msg_id
+  for byte in work_data: #checksum is stripped from the data
+    checksum += byte     #add up all the bytes
+
+  checksum = (checksum & 0xFF) + (checksum >> 8); #add upper and lower Bytes
+  checksum &= 0xFF #throw away anything in upper Byte
+  return checksum
+
+def calc_checksum_cruise(work_data):# 0x194 this checksum is special - initilized with 0
+  return calc_checksum_8bit(work_data, 0) 
+
+
 def create_accel_command(packer, action, bus, frame):
-    cnt2 = frame % 15 #cnt2 counts from 0 to 14
+    values = {
+        "setMe_0xFC": 0xFC,
+        "requests_0xF": 0xF,
+        "Counter_404": frame % 15 #counts from 0 to 14
+        }
     if action == "plus1":
-        cnt1_offset = 2
-        values = {"setMe_0xFC": 0xFC,
-                  "requests_0xF": 0xF,
-                  "plus1mph_request": 1,
-                  "notCancel_0xF": 0xF,
-                  "Counter1": (cnt2 - cnt1_offset) % 16, #BMW made counters weird - cnt1 counts to 15 but skips a step when cnt2 rolls to 0
-                  "Counter2": cnt2
-                  }
+        values["plus1mph_request"] = 1
     elif action == "minus1":
-        cnt1_offset = -1
-        values = {"setMe_0xFC": 0xFC,
-                  "requests_0xF": 0xF,
-                  "minus1mph_request": 1,
-                   "notCancel_0xF": 0xF,
-                  "Counter1": (cnt2 - cnt1_offset) % 16,
-                  "Counter2": cnt2
-                  }
+        values["minus1mph_request"] = 1
     elif action == "cancel":
-        cnt1_offset = 2
-        values = {"setMe_0xFC": 0xFC,
-                  "requests_0xF": 0xF,
-                  "Cancel_request_up_stalk": 1,
-                  "notCancel_0xF": 0x0,
-                  "Counter1": (cnt2 - cnt1_offset) % 16,
-                  "Counter2": cnt2
-                  }
+        values["Cancel_request_up_stalk"] = 1
+
+    dat = packer.make_can_msg("CruiseControl", bus, values)[2]
+    values["Checksum_404"] = calc_checksum_cruise(dat)
+
     # bus 0 - send on PT-CAN (JBBE <-> DME) works for V0540 only
     # bus 1 - send on F-CAN (SZL <-> DSC) works for both VO544 and V0540
     return packer.make_can_msg("CruiseControl", bus, values)
