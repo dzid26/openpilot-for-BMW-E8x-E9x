770c4ec22894e4f319b43465baa58ed926f0a77a
dzid26 <dzidmail@gmail.com>
2021-12-17 19:33:59 +0000

    Added more BMW signals from PT-CAN and F-CAN

--- selfdrive/car/bmw/carstate.py
+++ selfdrive/car/bmw/carstate.py
@@ -42,6 +42,7 @@ class CarState(CarStateBase):
     self.left_blinker_pressed = False
     self.otherButtons = False
     self.prev_gasPressed = False
+    self.sportMode = False
 
   def update(self, cp_PT, cp_F, cp_aux):
     # set these prev states at the beginning because they are used outside the update()
@@ -62,54 +63,75 @@ class CarState(CarStateBase):
     ret.brakePressed = cp_PT.vl["EngineAndBrake"]['BrakePressed'] != 0
     ret.gas = cp_PT.vl['AccPedal']["AcceleratorPedalPercentage"]
     ret.gasPressed = cp_PT.vl['AccPedal']["AcceleratorPedalPressed"] != 0
+    self.gas_kickdown = cp_PT.vl['AccPedal']["KickDownPressed"] != 0 #BMW has kickdown button at the bottom of the pedal
 
     ret.espDisabled = False  # cp.vl["ESP_CONTROL"]['TC_DISABLED'] ==1
 
     ret.vEgoRaw = cp_PT.vl['Speed']["VehicleSpeed"] * CV.KPH_TO_MS
-    ret.wheelSpeeds.fl = ret.vEgoRaw  # cp.vl["WHEEL_SPEEDS"]['WHEEL_SPEED_FL'] * CV.KPH_TO_MS #* speed_factor
-    ret.wheelSpeeds.fr = ret.vEgoRaw  # cp.vl["WHEEL_SPEEDS"]['WHEEL_SPEED_FR'] * CV.KPH_TO_MS #* speed_factor
-    ret.wheelSpeeds.fl = ret.vEgoRaw  # cp.vl["WHEEL_SPEEDS"]['WHEEL_SPEED_RL'] * CV.KPH_TO_MS #* speed_factor
-    ret.wheelSpeeds.fr = ret.vEgoRaw  # cp.vl["WHEEL_SPEEDS"]['WHEEL_SPEED_RR'] * CV.KPH_TO_MS #* speed_factor
+    ret.wheelSpeeds.fl = ret.vEgoRaw  # cp_PT.vl["WHEEL_SPEEDS"]['WHEEL_SPEED_FL'] * CV.KPH_TO_MS #* speed_factor
+    ret.wheelSpeeds.fr = ret.vEgoRaw  # cp_PT.vl["WHEEL_SPEEDS"]['WHEEL_SPEED_FR'] * CV.KPH_TO_MS #* speed_factor
+    ret.wheelSpeeds.fl = ret.vEgoRaw  # cp_PT.vl["WHEEL_SPEEDS"]['WHEEL_SPEED_RL'] * CV.KPH_TO_MS #* speed_factor
+    ret.wheelSpeeds.fr = ret.vEgoRaw  # cp_PT.vl["WHEEL_SPEEDS"]['WHEEL_SPEED_RR'] * CV.KPH_TO_MS #* speed_factor
     # ret.vEgoRaw = mean([ret.wheelSpeeds.fl, ret.wheelSpeeds.fr, ret.wheelSpeeds.rl, ret.wheelSpeeds.rr])
 
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
 
     ret.standstill = not ret.vEgoRaw > 0.001
 
-    ret.steeringAngle = (cp_F.vl['SteeringWheelAngle_DSC'][
-      'SteeringPosition'])  # slightly quicker on F-CAN  TODO find the factor and put in DBC
     ret.steeringRate = (cp_PT.vl["SteeringWheelAngle"]['SteeringSpeed'])
     can_gear = int(cp_PT.vl["TransmissionDataDisplay"]['ShiftLeverPosition'])
     ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(can_gear, None))
-    ret.leftBlinker = cp_PT.vl["TurnSignals"]['TurnSignalActive'] !=0 and cp_PT.vl["TurnSignals"]['LeftTurn'] !=0   # blinking
-    ret.rightBlinker = cp_PT.vl["TurnSignals"]['TurnSignalActive'] !=0  and cp_PT.vl["TurnSignals"]['RightTurn'] !=0   # blinking
-    self.right_blinker_pressed = cp_PT.vl["TurnSignals"]['RightTurn'] != 0
-    self.left_blinker_pressed = cp_PT.vl["TurnSignals"]['LeftTurn'] != 0
-    blinker_on = cp_PT.vl["TurnSignals"]['TurnSignalActive'] == 2
-    # self.blinker_off = cp_PT.vl["TurnSignals"]['TurnSignalIdle'] == 2
+    blinker_on = cp_PT.vl["TurnSignals"]['TurnSignalActive'] != 0 and cp_PT.vl["TurnSignals"]['TurnSignalIdle'] == 0
+    ret.leftBlinker = blinker_on and cp_PT.vl["TurnSignals"]['LeftTurn'] !=0   # blinking
+    ret.rightBlinker = blinker_on and cp_PT.vl["TurnSignals"]['RightTurn'] !=0   # blinking
+    self.right_blinker_pressed = not blinker_on and cp_PT.vl["TurnSignals"]['RightTurn'] != 0
+    self.left_blinker_pressed = not blinker_on and cp_PT.vl["TurnSignals"]['LeftTurn'] != 0
 
-    self.otherButtons = cp_PT.vl["SteeringButtons"]['Volume_DOWN'] !=0  or cp_PT.vl["SteeringButtons"]['Volume_UP'] !=0  or \
-                        cp_PT.vl["SteeringButtons"]['Previous_down'] !=0  or cp_PT.vl["SteeringButtons"]['Next_up'] !=0
-    ret.steeringTorque = 0
-    # do lane change after releasing blinker stalk
-    ret.steeringPressed = blinker_on and not self.right_blinker_pressed and not self.left_blinker_pressed
+    self.sportMode = cp_PT.vl["TransmissionDataDisplay"]['SportButtonState'] != 0
+    
+    self.otherButtons = \
+      cp_PT.vl["SteeringButtons"]['Volume_DOWN'] !=0  or cp_PT.vl["SteeringButtons"]['Volume_UP'] !=0  or \
+      cp_PT.vl["SteeringButtons"]['Previous_down'] !=0  or cp_PT.vl["SteeringButtons"]['Next_up'] !=0 \
+      or self.sportMode or \
+      self.prev_gasPressed and not ret.gasPressed # otherButtons used to indicate driver is engaged
+      # TODO: add other buttons (lights, gear, DTC, etc)
 
-    self.cruise_plus = cp_F.vl["CruiseControl"]['plus1mph_request'] != 0
-    self.cruise_minus = cp_F.vl["CruiseControl"]['minus1mph_request'] != 0
-    self.cruise_plus5 = cp_F.vl["CruiseControl"]['plus5mph_request'] != 0
-    self.cruise_minus5 = cp_F.vl["CruiseControl"]['minus5mph_request'] != 0
-    self.cruise_resume = cp_F.vl["CruiseControl"]['Resume_request'] != 0
-    self.cruise_cancel = cp_F.vl["CruiseControl"]['Cancel_request_up_or_down_stalk'] != 0
-    self.cruise_cancelUpStalk = cp_F.vl["CruiseControl"]['Cancel_request_up_stalk'] != 0
-    self.cruise_cancelDnStalk = self.cruise_cancel and not self.cruise_cancelUpStalk
+    # emulate driver steering torque - allows lane change assist on blinker hold
+    ret.steeringPressed = ret.gasPressed # E-series doesn't have torque sensor, so lightly pressing the gas indicates driver intention
+    if ret.steeringPressed and ret.leftBlinker:
+      ret.steeringTorque = 1
+    elif ret.steeringPressed and  ret.rightBlinker:
+      ret.steeringTorque = -1
+    else:
+      ret.steeringTorque = 0
 
-    ret.cruiseState.available = True  # todo unless DCS (traction control) disabled
-    if self.CP.carFingerprint in [CAR.E82_DCC, CAR.E90_DCC]:
+
+    ret.cruiseState.available = True #TODO cp_PT.vl['StatusDSC_KCAN']['DSC_full_off'] == 0 #cruise not available when DSC fully off
+    if self.CP.carFingerprint in [CAR.E82_DCC, CAR.E90_DCC]: #_DCC implies the F-CAN bus has to be connected
+      ret.steeringAngle = (cp_F.vl['SteeringWheelAngle_DSC']['SteeringPosition'])  # slightly quicker on F-CAN TODO find the factor and put in DBC
       ret.cruiseState.speed = cp_PT.vl["DynamicCruiseControlStatus"]['CruiseControlSetpointSpeed']
       ret.cruiseState.enabled = cp_PT.vl["DynamicCruiseControlStatus"]['CruiseActive'] != 0
+      # if we are sending on F-can, we also need to read on F-can to differentiate our messages from car messages
+      self.cruise_plus = cp_F.vl["CruiseControl"]['plus1mph_request'] != 0
+      self.cruise_minus = cp_F.vl["CruiseControl"]['minus1mph_request'] != 0
+      self.cruise_plus5 = cp_F.vl["CruiseControl"]['plus5mph_request'] != 0
+      self.cruise_minus5 = cp_F.vl["CruiseControl"]['minus5mph_request'] != 0
+      self.cruise_resume = cp_F.vl["CruiseControl"]['Resume_request'] != 0
+      self.cruise_cancel = cp_F.vl["CruiseControl"]['Cancel_request_up_or_down_stalk'] != 0
+      self.cruise_cancelUpStalk = cp_F.vl["CruiseControl"]['Cancel_request_up_stalk'] != 0
     elif self.CP.carFingerprint in [CAR.E82, CAR.E90]:
+      ret.steeringAngle = (cp_PT.vl['SteeringWheelAngle']['SteeringPosition'])
       ret.cruiseState.speed = cp_PT.vl["CruiseControlStatus"]['CruiseControlSetpointSpeed']
       ret.cruiseState.enabled = cp_PT.vl["CruiseControlStatus"]['CruiseCoontrolActiveFlag'] != 0
+      self.cruise_plus = cp_PT.vl["CruiseControl"]['plus1mph_request'] != 0
+      self.cruise_minus = cp_PT.vl["CruiseControl"]['minus1mph_request'] != 0
+      self.cruise_plus5 = cp_PT.vl["CruiseControl"]['plus5mph_request'] != 0
+      self.cruise_minus5 = cp_PT.vl["CruiseControl"]['minus5mph_request'] != 0
+      self.cruise_resume = cp_PT.vl["CruiseControl"]['Resume_request'] != 0
+      self.cruise_cancel = cp_PT.vl["CruiseControl"]['Cancel_request_up_or_down_stalk'] != 0
+      self.cruise_cancelUpStalk = cp_PT.vl["CruiseControl"]['Cancel_request_up_stalk'] != 0
+
+    self.cruise_cancelDnStalk = self.cruise_cancel and not self.cruise_cancelUpStalk
 
     if ret.cruiseState.enabled:
       if abs(ret.cruiseState.speed / ret.vEgoRaw - CV.MS_TO_KPH) < 0.3:
@@ -122,8 +144,8 @@ class CarState(CarStateBase):
     else:
       ret.cruiseState.speed = ret.cruiseState.speed * CV.MPH_TO_MS
 
-    ret.genericToggle = cp_PT.vl["TransmissionDataDisplay"]['SportButtonState'] != 0
-    ret.steeringTorqueEps = 0
+    ret.genericToggle = self.sportMode
+
 
     return ret
 
@@ -133,8 +155,10 @@ class CarState(CarStateBase):
       # sig_name, sig_address, default
       ("BrakePressed", "EngineAndBrake", 0),
       ("ShiftLeverPosition", "TransmissionDataDisplay", 0),
+      ("SportButtonState", "TransmissionDataDisplay", 0),
       ("AcceleratorPedalPressed", "AccPedal", 0),
       ("AcceleratorPedalPercentage", "AccPedal", 0),
+      ("KickDownPressed", "AccPedal", 0),
       ("VehicleSpeed", "Speed", 0),
       # ("DOOR_OPEN_FL", "SEATS_DOORS", 1),
       # ("DOOR_OPEN_FR", "SEATS_DOORS", 1),
@@ -185,15 +209,10 @@ class CarState(CarStateBase):
     return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 2)  # 1: F-CAN, 2: PT-CAN
 
   @staticmethod
-  def get_cam_can_parser( # comF-CAN
-    CP):  # 540 vehicle option could get away with just PT_CAN, but vo544 requires sending, and thus receiving, cruise commands on F-CAN. F-can works for both options.
+  def get_cam_can_parser(CP):  # 540 vehicle option could get away with just PT_CAN, but vo544 requires sending, and thus receiving, cruise commands on F-CAN. F-can works for both options.
     signals = [  # signal name, message name, default value
-      # sig_name, sig_address, default
-      ("SteeringPosition", "SteeringWheelAngle_DSC", 0),
-      ("Wheel1", "WheelSpeeds", 0),
-      ("Wheel2", "WheelSpeeds", 0),
-      ("Wheel3", "WheelSpeeds", 0),
-      ("Wheel4", "WheelSpeeds", 0),
+      ("SteeringPosition", "SteeringWheelAngle_DSC", 0), #this is slightly faster than PT-CAN equivalent
+      # some F-can messages, like CruiseControl, are mirrored on PT-CAN
       ("minus1mph_request", "CruiseControl", 0),
       ("plus1mph_request", "CruiseControl", 0),
       ("minus5mph_request", "CruiseControl", 0),
@@ -203,7 +222,12 @@ class CarState(CarStateBase):
       ("Cancel_request_up_or_down_stalk", "CruiseControl", 0),
     ]
 
-    checks = [  # refresh frequency Hz  TODO measure with PCAN
-      ("SteeringWheelAngle_DSC", 80),
-    ]
+    if CP.carFingerprint in [CAR.E82_DCC, CAR.E90_DCC]:
+      checks = [  # refresh frequency Hz  TODO measure with PCAN
+      ("SteeringWheelAngle_DSC", 80), ]
+    else:
+      checks = []
+
+    return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 1)  # 1: F-CAN,
+
     return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 1)  # 1: F-CAN, 2: PT-CAN
