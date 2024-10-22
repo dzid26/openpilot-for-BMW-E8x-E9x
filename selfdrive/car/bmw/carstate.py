14966adb24f46f7d05d971707f571f659f1c8580
oxygen <oxygen.liu@gmail.com>
2024-10-22 09:20:50 +0800

    Fix: steerFaultTemporary = True

--- selfdrive/car/bmw/carstate.py
+++ selfdrive/car/bmw/carstate.py
@@ -131,7 +131,7 @@ class CarState(CarStateBase):
       self.steer_angle_delta = cp_aux.vl['STEERING_STATUS']['STEERING_ANGLE']
       ret.steerFaultTemporary = int(cp_aux.vl['STEERING_STATUS']['CONTROL_STATUS']) & 0x4 != 0
     else:
-      ret.steerFaultTemporary = 1 # avoid Control Mismach error if STEPPER_SERVO_CAN is not available
+      ret.steerFaultTemporary = True # avoid Control Mismach error if STEPPER_SERVO_CAN is not available
 
     self.prev_gas_pressed = ret.gasPressed
     return ret
