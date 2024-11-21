fcdd241d723b7481a1a0a03fa15eb76fba43f05d
dzid26 <dzidmail@gmail.com>
2024-11-21 17:23:40 +0000

    more aggressive feedforward and P

--- selfdrive/car/bmw/interface.py
+++ selfdrive/car/bmw/interface.py
@@ -105,12 +105,11 @@ class CarInterface(CarInterfaceBase):
     ret.steerActuatorDelay = 0.6
     ret.steerLimitTimer = 0.4
     ret.lateralTuning.init('torque')
-    ret.lateralTuning.torque.kp = 1.0 / CarControllerParams.STEER_MAX
+    ret.lateralTuning.torque.kp = 1.5 / CarControllerParams.STEER_MAX
     ret.lateralTuning.torque.ki = 0.5 / CarControllerParams.STEER_MAX
-    ret.lateralTuning.torque.kf = 4.5 / CarControllerParams.STEER_MAX
+    ret.lateralTuning.torque.kf = 7.0 / CarControllerParams.STEER_MAX
     ret.lateralTuning.torque.friction = 0.23 #live parameters
     ret.lateralTuning.torque.latAccelFactor = 1.41 #live parameters
-    ret.lateralTuning.torque.latAccelOffset = -0.255
     ret.lateralTuning.torque.useSteeringAngle = False
     ret.lateralTuning.torque.steeringAngleDeadzoneDeg = 0.0 # backlash of stepper?
 
