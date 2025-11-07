3a1325793e6e9a1efa0db3f2bbf81203f9197d44
dzid26 <dzidmail@gmail.com>
2025-11-07 18:02:28 +0000

    SIMULATION=1

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -5,6 +5,7 @@ export FINGERPRINT="BMW_E82"
 export SKIP_FW_QUERY="1"
 export STARTED="1"
 export TESTING_CLOSET="1"
+export SIMULATION=1
 export ALLOW_DEBUG="1"
 export ZMQ=1
 exec ./launch_chffrplus.sh
